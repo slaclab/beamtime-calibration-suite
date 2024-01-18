c79fb58add62fe58467da40a906125cbfc871606
nstelter-slac <nstelter@slac.stanford.edu>
2024-01-18 09:38:06 -0800

    TST: Have simple test script handle ../scan_expected folder

--- run_simple_test.sh
+++ run_simple_test.sh
@@ -1,7 +1,17 @@
-# b4 running, make dir ../scan, and move scan_expected dir to ../scan_expected
-# results in ../scan will get overridden each run, so no need to delete
+# results in ../scan will get overridden each run, so no need to delete inbetween
+# if ../scan_expected exists, create if doesn't
+if [ ! -d "../scan_expected" ]; then
+    mkdir ../scan_expected
+fi
+
+# copy scan_expected/ to ../scan_expected only if they diff
+if ! diff -r scan_expected/ ../scan_expected/ >/dev/null 2>&1; then
+    cp -r scan_expected/* ../scan_expected/
+fi
+
 python EventScanParallelSlice.py -r 349 > out.txt # output noisy so hide
 python EventScanParallelSlice.py -r 349 -f ../scan/EventScanParallel_c0_r349_n1.h5 >> out.txt
+
 if diff -r ../scan ../scan_expected/ >/dev/null 2>&1; then
     echo "PASS"
 else
