11fa45cf2fb9ff54147f50c01dad9e51e0b57535
nstelter-slac <nstelter@slac.stanford.edu>
2024-02-20 11:03:51 -0800

    WIP: Move parsing and member variables already moved to psana2Base to pasna1Base

--- calibrationSuite/psana1Base.py
+++ calibrationSuite/psana1Base.py
@@ -1,5 +1,7 @@
 from psana import *
 from PSCalib.NDArrIO import load_txt
+from calibrationSuite.argumentParser import ArgumentParser
+from calibrationSuite.rixSuiteConfig import experimentHash
 
 class PsanaBase(object):
     def __init__(self, analysisType='scan'):
@@ -9,6 +11,115 @@ class PsanaBase(object):
         self.ePix10k_cameraTypes = {1:"Epix10ka", 4:"Epix10kaQuad", 16:"Epix10ka2M"}
         self.g0cut = 1<<14
         self.gainBitsMask = self.g0cut - 1
+
+        self.camera = 0
+        ##self.outputDir = '/sdf/data/lcls/ds/rix/rixx1003721/results/%s/' %(analysisType)
+        self.outputDir = '../%s/' %(analysisType)
+        ##self.outputDir = '/tmp'
+
+        self.className = self.__class__.__name__
+
+        try:
+            self.location = experimentHash['location']
+        except:
+            pass
+        try:
+            self.exp = experimentHash['exp']
+        except:
+            pass
+        try:
+        ##if True:
+            self.ROIfileNames = experimentHash['ROIs']
+            self.ROIs = []
+            for f in self.ROIfileNames:
+                self.ROIs.append(np.load(f+'.npy'))
+            try:  ## dumb code for compatibility or expectation
+                self.ROI = self.ROIs[0]
+            except:
+                pass
+        ##if False:
+        except:
+            print("had trouble finding", self.ROIfileNames)
+            self.ROI = None
+            self.ROIs = None
+        try:
+            self.singlePixels = experimentHash['singlePixels']
+        except:
+            self.singlePixels = None
+        try:
+            self.regionSlice = experimentHash['regionSlice']
+        except:
+            self.regionSlice = None
+        if self.regionSlice is not None:
+            self.sliceCoordinates = [[self.regionSlice[0].start,
+                                      self.regionSlice[0].stop],
+                                     [self.regionSlice[1].start,
+                                      self.regionSlice[1].stop],
+                                     ]
+            sc = self.sliceCoordinates
+            self.sliceEdges = [sc[0][1]-sc[0][0], sc[1][1]-sc[1][0]]
+
+        try:
+            self.fluxSource = experimentHash['fluxSource']
+            try:
+                self.fluxChannels = experimentHash['fluxChannels']
+            except:
+                self.fluxChannels = range(8,16) ## wave8
+            try:
+                self.fluxSign = experimentHash['fluxSign']
+            except:
+                self.fluxSign = 1
+        except:
+            self.fluxSource = None
+
+        ## for non-120 Hz running
+        self.nRunCodeEvents = 0
+        self.nDaqCodeEvents = 0
+        self.nBeamCodeEvents = 0
+        self.runCode = 280
+        self.daqCode = 281
+        self.beamCode = 283 ## per Matt
+        ##self.beamCode = 281 ## don't see 283...
+        self.fakeBeamCode = False
+
+        # Parse command-line arguments
+        args_parser = ArgumentParser()
+        args = args_parser.parse_args()
+
+        # Handle cmdline arguments
+        self.file = args.file
+        self.label = args.label or ""
+
+        # Run number
+        self.run = args.run if args.run is not None else None
+        self.camera = args.camera if args.camera is not None else self.camera
+        # Experiment name
+        self.exp = args.exp if args.exp is not None else self.exp
+        self.location = args.location if args.location is not None else self.location
+        self.maxNevents = args.maxNevents if args.maxNevents is not None else None
+        self.skipNevents = args.skipNevents if args.skipNevents is not None else None
+        self.outputDir = args.path if args.path is not None else self.outputDir
+        self.detObj = args.detObj
+        self.threshold = eval(args.threshold) if args.threshold is not None else None
+        self.fluxCut = args.fluxCut if args.fluxCut is not None else None
+        try:
+            self.runRange = eval(args.runRange)  ## in case needed
+        except Exception as e:
+            print(f"An exception occurred: {e}")
+            self.runRange = None
+        self.fivePedestalRun = args.fivePedestalRun if args.fivePedestalRun is not None else None
+        self.fakePedestal = args.fakePedestal if args.fakePedestal is not None else None
+        self.fakePedestalFrame = np.load(self.fakePedestal) if self.fakePedestal is not None else None
+        if args.detType == '':
+            ## assume epix10k for now
+            if args.nModules is not None:
+                self.detType = self.ePix10k_cameraTypes[args.nModules]
+        else:
+            self.detType = args.detType
+        self.special = args.special
+        self.ds = None
+        self.det = None ## do we need multiple dets in an array? or self.secondDet?
+        # Done handling cmdline arguments
 
         ## self.setupPsana()
 
