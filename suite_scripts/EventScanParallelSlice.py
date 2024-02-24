88394f3fbfeaf7d6df7fb1d3e6668f80d142d6da
nstelter-slac <nstelter@slac.stanford.edu>
2024-02-24 13:58:11 -0800

    WIP: move logging setup into its own library file

--- suite_scripts/EventScanParallelSlice.py
+++ suite_scripts/EventScanParallelSlice.py
@@ -1,17 +1,12 @@
 from calibrationSuite.basicSuiteScript import *
 import logging
-
-# Setup logging to file '<filename>.log', and configures the underlying calibration-library logging.
-# Log file gets appended to each new run, and can manually delete for fresh log.
-# (Could change so makes new unique log each run or overwrites existing log.)
-logging.basicConfig(
-    filename= os.path.basename(__file__)[:-3] + ".log",
-    level=logging.INFO, # For full logging set to INFO which includes ERROR logging too
-    format='%(asctime)s - %(levelname)s - %(message)s' # levelname is log severity (ERROR, INFO, etc)
-)
-
 # for logging from current file
 logger = logging.getLogger(__name__)
+
+import calibrationSuite.loggingSetup as ls
+import os
+currFileName = os.path.basename(__file__)
+ls.setupScriptLogging(currFileName[:-3] + ".log", logging.INFO)
 
 class EventScanParallel(BasicSuiteScript):
     def __init__(self):
