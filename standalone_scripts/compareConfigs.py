75ddc4726b5854c049f3a609376d90b3863e0e29
nstelter-slac <nstelter@slac.stanford.edu>
2024-03-04 17:39:46 -0800

    WIP: Apply black formatting to standalone_scripts

--- standalone_scripts/compareConfigs.py
+++ standalone_scripts/compareConfigs.py
@@ -1,27 +1,30 @@
 import sys
 
-c0 = open(sys.argv[1], 'r')
-c1 = open(sys.argv[2], 'r')
+c0 = open(sys.argv[1], "r")
+c1 = open(sys.argv[2], "r")
 
 lines0 = c0.readlines()
 lines1 = c1.readlines()
 
 for l0 in lines0:
-    if not l0.startswith('config'): continue
-    if l0.startswith('step 1'): break
+    if not l0.startswith("config"):
+        continue
+    if l0.startswith("step 1"):
+        break
     try:
         r0, v0 = l0.split()
     except:
-##        print(l0)
+        ##        print(l0)
         continue
 
     for l1 in lines1:
-        if not l1.startswith('config'): continue
-        if l1.startswith('step 1'): break
+        if not l1.startswith("config"):
+            continue
+        if l1.startswith("step 1"):
+            break
         try:
             r1, v1 = l1.split()
         except:
             continue
         if r1 == r0 and v1 != v0:
-            print("%s: %s -> %s" %(r0, v0, v1))
-
+            print("%s: %s -> %s" % (r0, v0, v1))
