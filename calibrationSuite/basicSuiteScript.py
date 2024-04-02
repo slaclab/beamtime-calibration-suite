93f40136c77f4b98936b028d0ac41b4a4d268be6
philiph-slac <philiph@sdfiana001.sdf.slac.stanford.edu>
2024-04-02 14:00:54 -0700

    Had wrong directory structure in rixsCCD

--- calibrationSuite/basicSuiteScript.py
+++ calibrationSuite/basicSuiteScript.py
@@ -45,8 +45,6 @@ class BasicSuiteScript(PsanaBase):
         self.camera = 0
         ##self.outputDir = '/sdf/data/lcls/ds/rix/rixx1003721/results/%s/' %(analysisType)
         self.outputDir = "../%s/" % (analysisType)
-        print("this messes things up")
-        self.outputDir = "/%s/" % (analysisType)
         logging.info("output dir: " + self.outputDir)
         ##self.outputDir = '/tmp'
 
