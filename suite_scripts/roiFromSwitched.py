be8ff0068e0c38e821fabd42253bcc6c7d766897
nstelter-slac <nstelter@slac.stanford.edu>
2024-05-21 18:27:30 -0700

    WIP: cleaning up roiSwitch script for adding to tests

--- suite_scripts/roiFromSwitched.py
+++ suite_scripts/roiFromSwitched.py
@@ -7,43 +7,59 @@
 ## may be copied, modified, propagated, or distributed except according to
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
-from calibrationSuite.basicSuiteScript import *
+import numpy as np
+import calibrationSuite.loggingSetup as ls
+import logging
+import os
 
+from calibrationSuite.basicSuiteScript import BasicSuiteScript
 
-class RoiFromSwitched(BasicSuiteScript):
-    def __init__(self):
-        super().__init__()  ##self)
-
+# for logging from current file
+logger = logging.getLogger(__name__)
+# log to file named <curr script name>.log
+currFileName = os.path.basename(__file__)
+ls.setupScriptLogging(
+    "../logs/" + currFileName[:-3] + ".log", logging.ERROR
+)  # change to logging.INFO for full logging output
 
 if __name__ == "__main__":
-    rfs = RoiFromSwitched()
+
+    rfs = BasicSuiteScript("test2")
     print("have built an RFS")
+    logger.info("have built an RFS")
+
     rfs.setupPsana()
+
     switchedPixels = None
     nGoodEvents = 0
     while True:
         evt = rfs.getEvt()
         if evt is None:
             break
+
         rawFrames = rfs.getRawData(evt, gainBitsMasked=False)
         if rawFrames is None:
-            print("No contrib found")
             continue
 
         fG1 = rawFrames >= rfs.g0cut
         try:
             switchedPixels = np.bitwise_or(fG1, switchedPixels)
-        except:
-            ##print("first time hopefully")
-            switchedPixels = fG1  ##copy?
+        except Exception:
+            switchedPixels = fG1
 
         nGoodEvents += 1
         if nGoodEvents % 100 == 0:
             print("n good events analyzed: %d" % (nGoodEvents))
+            logger.info("n good events analyzed: %d" % (nGoodEvents))
             print("switched pixels: %d" % ((switchedPixels > 0).sum()))
+            logger.info("switched pixels: %d" % ((switchedPixels > 0).sum()))
 
         if nGoodEvents > rfs.maxNevents:
             break
 
-    np.save("roiFromSwitched_r%d_c%d.npy" % (rfs.run, rfs.camera), switchedPixels)
-    print("%d pixels were in low at least once in %d events" % ((switchedPixels > 0).sum()))
+    fileName = "%sroiFromSwitched_r%d_c%d.npy" % (rfs.outputDir, rfs.run, rfs.camera)
+    np.save(fileName, switchedPixels)
+    logger.info("Wrote file: ", fileName)
+
+    print("%d pixels were in low at least once in %d events" % ((switchedPixels > 0).sum(), nGoodEvents))
+    logger.info("%d pixels were in low at least once in %d events" % ((switchedPixels > 0).sum(), nGoodEvents))
