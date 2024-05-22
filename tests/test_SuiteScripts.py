89a832bd10b2dd42b812a24f654c10ab08295915
nstelter-slac <nstelter@slac.stanford.edu>
2024-05-21 17:27:57 -0700

    WIP: working on suite_scripts tests

--- tests/test_SuiteScripts.py
+++ tests/test_SuiteScripts.py
@@ -1,24 +1,27 @@
 import os
 import subprocess
 import filecmp
+import pytest
 
-def test_calculation():
-    # Run the script with the specified command
-    #result = subprocess.run(['python', '../suite_scripts/CalcNoiseAndMean.py', '-r', '102'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = subprocess.run(['bash', '-c', 'cd ../ && source setup.sh && cd suite_scripts  && python CalcNoiseAndMean.py -r 102'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-   
-    print(result.stdout)
-    # Check if the script ran successfully
+def run_command(command):
+    print("cmd: ", command)
+    result = subprocess.run(command, capture_output=True, text=True)
+    return result
+
+@pytest.mark.parametrize("command, real_output_location, expected_output_location", [
+    (['bash', '-c', 'cd ../ && source setup.sh && cd suite_scripts  && python CalcNoiseAndMean.py -r 102'],
+     '../suite_scripts/dark',
+     './test_data/calc_noise_mean_no_common'),
+    (['bash', '-c', 'cd ../ && source setup.sh && cd suite_scripts  && python CalcNoiseAndMean.py -r 102'],
+     '../suite_scripts/dark',
+     './test_data/calc_noise_mean_no_common'),
+])
+def test_calculation(command, real_output_location, expected_output_location):
+    result = run_command(command)
+    #print(result.stdout)
     assert result.returncode == 0, f"Script failed with error: {result.stderr.decode()}"
 
-    # Define the paths to the 'dark' and 'expected' folders
-    dark_folder = '../suite_scripts/dark'
-    expected_folder = './test_data/calc_noise_mean_no_common'
-    
-    # Compare the contents of the 'dark' folder with the 'expected' folder
-    folders_equal = filecmp.dircmp(dark_folder, expected_folder)
-
-    # Check if the folders are identical
-    assert folders_equal.left_only == [], f"Files missing in 'dark' folder: {folders_equal.left_only}"
-    assert folders_equal.right_only == [], f"Extra files in 'dark' folder: {folders_equal.right_only}"
+    folders_equal = filecmp.dircmp(real_output_location, expected_output_location)
+    assert folders_equal.left_only == [], f"Files missing in 'real' folder: {folders_equal.left_only}"
+    assert folders_equal.right_only == [], f"Extra files in 'real' folder: {folders_equal.right_only}"
     assert folders_equal.diff_files == [], f"Differences found in files: {folders_equal.diff_files}"
