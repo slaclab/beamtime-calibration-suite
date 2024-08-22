f0ffa7dcd0b983bc05c15869ddd98b6822879426
philiph-slac <philiph@sdfiana003.sdf.slac.stanford.edu>
2024-08-22 11:56:38 -0700

    Added setup scripts for different detectors and some gain analysis support scripts - should maybe move both

---
+++ setup_jf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Setup environment variables
+
+
+# current_dir=$(pwd)
+git_project_root_dir=$(git rev-parse --show-toplevel)
+# so scripts can find the calibrationSuite library code
+export PYTHONPATH="$PYTHONPATH:$git_project_root_dir"
+echo "PYTHONPATH = $PYTHONPATH"
+
+# so output folders are written in a shared location
+##export OUTPUT_ROOT="/sdf/data/lcls/ds/rix/rixx1005922/scratch/"
+##export OUTPUT_ROOT="/sdf/data/lcls/ds/rix/rixx1005922/results/"
+export OUTPUT_ROOT="/sdf/data/lcls/ds/cxi/cxic00121/results/offsetCalibration/"
+echo "OUTPUT_ROOT = $OUTPUT_ROOT"
+
+# point to which config file to use
+##export SUITE_CONFIG="$git_project_root_dir/config_files/epixMSuiteConfig.py"
+export SUITE_CONFIG="$git_project_root_dir/config_files/jfSuiteConfig.py"
+echo "SUITE_CONFIG = $SUITE_CONFIG"
---
+++ suite_scripts/makeGainCorrection.py
@@ -0,0 +1,69 @@
+##############################################################################
+## This file is part of 'SLAC Beamtime Calibration Suite'.
+## It is subject to the license terms in the LICENSE.txt file found in the
+## top-level directory of this distribution and at:
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+## No part of 'SLAC Beamtime Calibration Suite', including this file,
+## may be copied, modified, propagated, or distributed except according to
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+import sys
+
+import numpy as np
+
+from calibrationSuite.basicSuiteScript import BasicSuiteScript
+from calibrationSuite import ancillaryMethods
+
+
+class MakeGainCorrection(BasicSuiteScript):
+    def __init__(self):
+        super().__init__()  ##self)
+
+if __name__ == "__main__":
+    mgc = MakeGainCorrection()
+    mgc.setupPsana()
+
+    if mgc.fakePedestal is not None:
+        pedestal = mgc.fakePedestal
+    else:
+        evt = mgc.getEvt()
+        pedestal = mgc.getPedestal(evt, gainmode=0)
+    ## pedestal should be the same shape as a the numpy file analyzed
+
+    data = np.load(mgc.file)
+    print("found %d NaN elements in %s" %(np.isnan(data).sum(), mgc.file))
+
+    ## handle 2d case
+    if mgc.detectorInfo.dimension == 2:
+        data = np.array([data])
+        pedestal = np.array([pedestal])
+
+    ## clean up by module
+    ## ??? or bank ???
+    for m in range(mgc.detectorInfo.nModules):
+        regionMedian = np.median(data[m][np.isfinite(data[m])])
+        data[m][np.isnan(data[m])] = regionMedian
+        rms = data[m].std()
+        data[m] = data[m].clip(regionMedian-3*rms, regionMedian+3*rms)
+        print("have clipped module %d, rms was %0.2f, now %0.2f"
+              %(m, rms, data[m].std()))
+
+    correction = measuredAduPerKeV = data/mgc.photonEnergy*mgc.detectorInfo.aduPerKeV
+    if mgc.detectorInfo.detectorType == "Epix100a":
+        ## psana wants keV/adu in this one case
+        correction = 1./measuredAduPerKeV
+
+    fileName = mgc.file.split(".npy")[0] + "_cleanedCorrection.npy"
+    np.save(fileName, correction)
+    
+    print("n.b. for autoranging detectors this needs to be applied carefully")
+
+    import matplotlib.pyplot as plt
+    pedMean = pedestal.mean()
+    pedRms = pedestal.std()
+    x, y, err = ancillaryMethods.makeProfile(pedestal.clip(pedMean-5*pedRms,pedMean+4*pedRms).flatten(), correction.flatten(), 50)##, spread=True)##, myStatistic="median")
+    plt.errorbar(x, y, err)
+    plt.xlabel("pedestal")
+    plt.ylabel("gain correction")
+    fileName = mgc.file.split(".npy")[0] + "_correctionVsPed.png"
+    plt.savefig(fileName)
